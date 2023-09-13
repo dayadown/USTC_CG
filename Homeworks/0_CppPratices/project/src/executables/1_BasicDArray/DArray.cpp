ba281557da2ad479f923cc9ecb7288230f7628a7
dayadown <1593441796@qq.com>
2023-09-13 20:18:01 +0800

    basic_darray

--- Homeworks/0_CppPratices/project/src/executables/1_BasicDArray/DArray.cpp
+++ Homeworks/0_CppPratices/project/src/executables/1_BasicDArray/DArray.cpp
@@ -1,6 +1,7 @@
 // implementation of class DArray
 #include "DArray.h"
-
+#include<iostream>
+using namespace std;
 // default constructor
 DArray::DArray() {
 	Init();
@@ -8,11 +9,17 @@ DArray::DArray() {
 
 // set an array with default values
 DArray::DArray(int nSize, double dValue) {
-	//TODO
+	m_pData = new double[nSize];
+	m_nSize = nSize;
+	for (int i = 0; i < nSize; i++)
+		m_pData[i] = dValue;
 }
 
 DArray::DArray(const DArray& arr) {
-	//TODO
+	m_nSize = arr.GetSize();
+	m_pData = new double[m_nSize];
+	for (int i = 0; i < m_nSize; i++)
+		m_pData[i] = arr[i];
 }
 
 // deconstructor
@@ -22,66 +29,89 @@ DArray::~DArray() {
 
 // display the elements of the array
 void DArray::Print() const {
-	//TODO
+	if (m_nSize == 0) {
+		cout << "元素个数为0！" << endl;
+		return;
+	}
+	for (int i = 0; i < m_nSize; i++)
+		cout << m_pData[i] << " ";
+	cout << endl;
 }
 
 // initilize the array
 void DArray::Init() {
-	//TODO
+	m_pData = nullptr;
+	m_nSize = 0;
 }
 
 // free the array
 void DArray::Free() {
-	//TODO
+	delete[] m_pData;
+	m_pData = nullptr;
+	m_nSize = 0;
 }
 
 // get the size of the array
 int DArray::GetSize() const {
-	//TODO
-	return 0; // you should return a correct value
+	return m_nSize;
 }
 
 // set the size of the array
 void DArray::SetSize(int nSize) {
-	//TODO
+	if (nSize <= m_nSize)
+		m_nSize = nSize;
+	else {
+		double* arr = new double[nSize];
+		for (int i = 0; i < m_nSize; i++)
+			arr[i] = m_pData[i];
+		for (int i = m_nSize; i < nSize; i++)
+			arr[i] = 0.;
+		delete[] m_pData;
+		m_pData = arr;
+		m_nSize = nSize;
+	}
 }
 
 // get an element at an index
 const double& DArray::GetAt(int nIndex) const {
-	//TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	return m_pData[nIndex];
 }
 
 // set the value of an element 
 void DArray::SetAt(int nIndex, double dValue) {
-	//TODO
+	m_pData[nIndex] = dValue;
 }
 
 // overload operator '[]'
 const double& DArray::operator[](int nIndex) const {
-	//TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	return m_pData[nIndex];
 }
 
 // add a new element at the end of the array
 void DArray::PushBack(double dValue) {
-	//TODO
+	InsertAt(m_nSize, dValue);
 }
 
 // delete an element at some index
 void DArray::DeleteAt(int nIndex) {
-	//TODO
+	for (int i = nIndex; i < m_nSize - 1; i++)
+		m_pData[i] = m_pData[i + 1];
+	m_nSize--;
 }
 
 // insert a new element at some index
 void DArray::InsertAt(int nIndex, double dValue) {
-	//TODO
+	SetSize(m_nSize + 1);
+	for (int i = m_nSize - 1; i > nIndex; i--)
+		m_pData[i] = m_pData[i - 1];
+	m_pData[nIndex] = dValue;
 }
 
 // overload operator '='
 DArray& DArray::operator = (const DArray& arr) {
-	//TODO
+	m_nSize = arr.GetSize();
+	m_pData = new double[m_nSize];
+	for (int i = 0; i < m_nSize; i++)
+		m_pData[i] = arr[i];
 	return *this;
 }
