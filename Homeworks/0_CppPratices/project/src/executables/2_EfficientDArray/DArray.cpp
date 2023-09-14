f1f60b8b040b5c66b1bc1de9417c67ead5b1bb92
dayadown <1593441796@qq.com>
2023-09-14 17:01:25 +0800

    efficient_darray

--- Homeworks/0_CppPratices/project/src/executables/2_EfficientDArray/DArray.cpp
+++ Homeworks/0_CppPratices/project/src/executables/2_EfficientDArray/DArray.cpp
@@ -1,18 +1,25 @@
 // implementation of class DArray
 #include "DArray.h"
-
+#include<iostream>
+using namespace std;
 // default constructor
 DArray::DArray() {
 	Init();
 }
 
 // set an array with default values
-DArray::DArray(int nSize, double dValue) {
-	//TODO
+DArray::DArray(int nSize, double dValue)
+	:m_pData(new double[nSize]), m_nMax(nSize),m_nSize(nSize)
+{
+	for (int i = 0; i < nSize; i++)
+		m_pData[i] = dValue;
 }
 
-DArray::DArray(const DArray& arr) {
-	//TODO
+DArray::DArray(const DArray& arr)
+	:m_nMax(arr.m_nMax), m_nSize(arr.m_nSize), m_pData(new double[arr.m_nMax])
+{
+	for (int i = 0; i < m_nSize; i++)
+		m_pData[i] = arr[i];
 }
 
 // deconstructor
@@ -22,73 +29,105 @@ DArray::~DArray() {
 
 // display the elements of the array
 void DArray::Print() const {
-	//TODO
+	if (m_nSize == 0)
+		cout << "no element";
+	for (int i = 0; i < m_nSize; i++)
+		cout << m_pData[i] << " ";
+	cout << endl;
 }
 
 // initilize the array
 void DArray::Init() {
-	//TODO
+	m_nMax = m_nSize = 0;
+	m_pData = nullptr;
 }
 
 // free the array
 void DArray::Free() {
-	//TODO
+	m_nMax = m_nSize = 0;
+	delete[] m_pData;
+	m_pData = nullptr;
 }
 
 // get the size of the array
 int DArray::GetSize() const {
-	//TODO
-	return 0; // you should return a correct value
+	return m_nMax;
 }
 
 // set the size of the array
 void DArray::SetSize(int nSize) {
-	//TODO
+	if (nSize == m_nMax)
+		return;
+	double* pdata = new double[nSize];
+	for (int i = 0; i < nSize && i < m_nSize; i++)
+		pdata[i] = m_pData[i];
+	if (nSize > m_nSize)
+		m_nMax = nSize;
+	else
+		m_nMax = m_nSize;
+	delete[] m_pData;
+	m_pData = pdata;
 }
 
 // get an element at an index
 const double& DArray::GetAt(int nIndex) const {
-	//TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	return m_pData[nIndex];
 }
 
 // set the value of an element 
 void DArray::SetAt(int nIndex, double dValue) {
-	//TODO
+	if (nIndex < m_nMax)
+		m_pData[nIndex] = dValue;
+	else {
+		SetSize(m_nMax * 2);
+		m_pData[nIndex] = dValue;
+	}
 }
 
 // overload operator '[]'
 double& DArray::operator[](int nIndex) {
-	// TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	return m_pData[nIndex];
 }
 
 // overload operator '[]'
 const double& DArray::operator[](int nIndex) const {
-	//TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	return m_pData[nIndex];
 }
 
 // add a new element at the end of the array
 void DArray::PushBack(double dValue) {
-	//TODO
+	if (m_nMax > m_nSize)
+		m_pData[m_nSize++] = dValue;
+	else {
+		SetSize((m_nSize+1) * 2);
+		m_nMax = (m_nSize+1) * 2;
+		m_pData[m_nSize++] = dValue;
+	}
 }
 
 // delete an element at some index
 void DArray::DeleteAt(int nIndex) {
-	//TODO
+	for (int i = nIndex; i < m_nSize - 1; i++)
+		m_pData[i] = m_pData[i + 1];
+	m_nSize--;
 }
 
 // insert a new element at some index
 void DArray::InsertAt(int nIndex, double dValue) {
-	//TODO
+	if (m_nMax == m_nSize)
+		SetSize((m_nSize + 1)*2);
+	for (int i = m_nSize; i > nIndex; i--)
+		m_pData[i] = m_pData[i - 1];
+	m_pData[nIndex] = dValue;
+	m_nSize++;
 }
 
 // overload operator '='
 DArray& DArray::operator = (const DArray& arr) {
-	//TODO
+	m_nMax = arr.GetSize();
+	m_pData = new double[m_nMax];
+	for (int i = 0; i < arr.m_nSize; i++)
+		m_pData[i] = arr[i];
+	m_nSize = arr.m_nSize;
 	return *this;
 }
--- Homeworks/0_CppPratices/project/src/executables/2_EfficientDArray/main.cpp
+++ Homeworks/0_CppPratices/project/src/executables/2_EfficientDArray/main.cpp
@@ -4,17 +4,14 @@ int main(int argc, char** argv) {
 	DArray a;
 	a.InsertAt(0, 2.1);
 	a.Print();
-
 	a.PushBack(3.0);
 	a.PushBack(3.1);
 	a.PushBack(3.2);
 	a.Print();
-
 	a.DeleteAt(0);
 	a.Print();
 	a.InsertAt(0, 4.1);
 	a.Print();
-
 	DArray acopy = a; // 此处用到了拷贝构造函数
 	acopy.Print();
 
@@ -25,7 +22,6 @@ int main(int argc, char** argv) {
 	acopy4 = acopy3 = a; // 此处用到了赋值操作符号"="的重载
 	acopy3.Print();
 	acopy4.Print();
-
 	DArray b;
 	b.PushBack(21);
 	b.Print();
@@ -41,6 +37,5 @@ int main(int argc, char** argv) {
 	c.PushBack('c');
 	c.InsertAt(0, 'd');
 	c.Print();
-
 	return 0;
 }
